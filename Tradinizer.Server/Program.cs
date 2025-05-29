1bad3d27479e78ef496e3921a24d17df04d9a2f0
GoldenEye <follo.85@gmail.com>
2025-05-29 08:44:10 +0200

    Delete Usless Files

--- Tradinizer.Server/Program.cs
+++ Tradinizer.Server/Program.cs
@@ -1,4 +1,6 @@
 ﻿
+using Microsoft.Extensions.FileProviders;
+
 namespace Tradinizer.Server
 {
     public class Program
@@ -13,11 +15,25 @@ namespace Tradinizer.Server
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
 
+            int port = builder.Configuration.GetValue<int>("AppSettings:Port", 5000);
+
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenLocalhost(port);
+            });
+
             var app = builder.Build();
 
+            var viteDistPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..", "tradinizer.client", "dist"));
+
             app.UseDefaultFiles();
-            app.MapStaticAssets();
-            app.UseStaticFiles();
+            //app.MapStaticAssets();
+            //app.UseStaticFiles();
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(viteDistPath),
+                RequestPath = ""
+            });
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
@@ -32,7 +48,12 @@ namespace Tradinizer.Server
 
             app.MapControllers();
 
-            app.MapFallbackToFile("/index.html");
+            //app.MapFallbackToFile("/index.html");
+
+            app.MapFallbackToFile("index.html", new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(viteDistPath)
+            });
 
             app.Run();
         }
